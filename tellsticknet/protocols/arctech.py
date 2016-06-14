4f16524e33053b11ee214c44cbd4bead0ddc31f2
Erik <erik.eriksson@yahoo.com>
2016-06-14 17:21:57 +0200

    added implementation

--- tellsticknet/protocols/arctech.py
+++ tellsticknet/protocols/arctech.py
@@ -1,8 +1,37 @@
-def decode(data):
+def decode(data, args):
     """
     https://github.com/telldus/telldus/blob/master/telldus-core/service/ProtocolNexa.cpp
     """
-    raise NotImplementedError()
+    if args["model"] != "selflearning":
+        raise NotImplementedError()
+
+    house = data & 0xFFFFFFC0
+    house >>= 6
+
+    group = data & 0x20
+    group >>= 5
+
+    method = data & 0x10
+    method >>= 4
+
+    unit = data & 0xF
+    unit += 1
+
+    if house < 1 or house > 67108863 or unit < 1 or unit > 16:
+        return
+
+    if method == 1:
+        method = "turnon"
+    elif method == 0:
+        method = "turnoff"
+    else:
+        raise RuntimeError("invalid method", method)
+
+    return dict(_class="command",
+                house=house,
+                unit=unit,
+                group=group,
+                method=method)
 
 
 def encode(what):
