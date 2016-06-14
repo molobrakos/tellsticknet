48ba0dc8ac519885184fdd93d74cd3105fdd734b
Erik <erik.eriksson@yahoo.com>
2016-06-14 17:22:07 +0200

    added implementation

--- tellsticknet/protocols/everflourish.py
+++ tellsticknet/protocols/everflourish.py
@@ -1,4 +1,42 @@
-def decode(data):
+import logging
+_LOGGER = logging.getLogger(__name__)
+
+
+def decode(data, args):
+    """
+    https://github.com/telldus/telldus/blob/master/telldus-core/service/ProtocolEverflourish.cpp
+    """
+
+    house = data & 0xFFFC00
+    house >>= 10
+
+    unit = data & 0x300
+    unit >>= 8
+    unit += 1
+
+    method = data & 0xF
+
+    if house > 16383 or unit < 1 or unit > 4:
+        # not everflourish
+        return None
+
+    if method == 0:
+        method = "turnoff"
+    elif method == 15:
+        method = "turnon"
+    elif method == 10:
+        method = "learn"
+    else:
+        raise RuntimeError("invalid method", method)
+
+    return dict(_class="command",
+                model="selflearning",
+                house=house,
+                unit=unit,
+                method=method)
+
+
+def encode(method):
     """
     https://github.com/telldus/telldus/blob/master/telldus-core/service/ProtocolEverflourish.cpp
     """
