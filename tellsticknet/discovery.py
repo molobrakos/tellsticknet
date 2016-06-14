9facddac52638cf4de004b056090ae6cac4cc9f9
Erik <erik.eriksson@yahoo.com>
2016-06-14 14:18:47 +0200

    capitalized logging messages

--- tellsticknet/discovery.py
+++ tellsticknet/discovery.py
@@ -14,7 +14,7 @@ _LOGGER = logging.getLogger(__name__)
 
 def discover(timeout=DISCOVERY_TIMEOUT):
     """Scan network for Tellstick Net devices"""
-    _LOGGER.info("discovering tellstick devices")
+    _LOGGER.info("Discovering tellstick devices ...")
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
