bdb570e48c333e09c1831677748291a4e73ba7d2
Erik <erik.eriksson@yahoo.com>
2016-06-17 14:08:37 +0200

    extracted into nexa, sartano, waveman

--- tellsticknet/protocols/arctech.py
+++ tellsticknet/protocols/arctech.py
@@ -1,37 +1,13 @@
+import logging
+_LOGGER = logging.getLogger(__name__)
+
+from . import nexa, waveman, sartano
+
 def decode(data, args):
-    """
-    https://github.com/telldus/telldus/blob/master/telldus-core/service/ProtocolNexa.cpp
-    """
-    if args["model"] != "selflearning":
-        raise NotImplementedError()
-
-    house = data & 0xFFFFFFC0
-    house >>= 6
-
-    group = data & 0x20
-    group >>= 5
-
-    method = data & 0x10
-    method >>= 4
-
-    unit = data & 0xF
-    unit += 1
-
-    if house < 1 or house > 67108863 or unit < 1 or unit > 16:
-        return
-
-    if method == 1:
-        method = "turnon"
-    elif method == 0:
-        method = "turnoff"
-    else:
-        raise RuntimeError("invalid method", method)
-
-    return dict(_class="command",
-                house=house,
-                unit=unit,
-                group=group,
-                method=method)
+    print("ok")
+    return nexa.decode(data, args) or \
+        waveman.decode(data, args) or \
+        sartano.decode(data, args)
 
 
 def encode(what):
@@ -46,3 +22,4 @@ def encode(what):
     (len(protocol), protocol, len(model), model, house, unit, method)
     """
     pass
+
