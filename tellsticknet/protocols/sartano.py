00688473cf2df8ef26bff0a1685abf55595759b2
Erik Eriksson <molobrakos@users.noreply.github.com>
2016-06-19 13:18:57 +0200

    reference source

--- tellsticknet/protocols/sartano.py
+++ tellsticknet/protocols/sartano.py
@@ -1,6 +1,7 @@
 import logging
 _LOGGER = logging.getLogger(__name__)
 
+# https://github.com/telldus/telldus/blob/master/telldus-core/service/ProtocolSartano.cpp
 
 def decode(data_in, args):
     data = 0
