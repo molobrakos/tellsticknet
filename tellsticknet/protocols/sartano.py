45814d98a4c234c7adec1145a4b8fe05eeae4ef9
Erik <erik.eriksson@yahoo.com>
2016-06-22 08:50:44 +0200

    re-added code parsing. adapted to new interface

--- tellsticknet/protocols/sartano.py
+++ tellsticknet/protocols/sartano.py
@@ -4,14 +4,18 @@ _LOGGER = logging.getLogger(__name__)
 # https://github.com/telldus/telldus/blob/master/telldus-core/service/ProtocolSartano.cpp
 
 
-def decode(data_in, args):
-    data = 0
+def decode(packet):
+    data = packet.pop("data")
+
+    data2 = 0
     mask = 1 << 11
     for i in range(0, 12):
-        data >>= 1
-        if data_in & mask == 0:
-            data |= (1 << 11)
+        data2 >>= 1
+        if data & mask == 0:
+            data2 |= (1 << 11)
         mask >>= 1
+
+    data = data2
 
     code = data & 0xFFC
     code >>= 2
@@ -32,7 +36,18 @@ def decode(data_in, args):
         _LOGGER.debug("Not Sartano")
         return
 
-    ret = dict(_class="command",
+    mask = 1 << 9
+    code2 = ""
+    for i in range(0, 10):
+        if code & mask != 0:
+            code2 += "1"
+        else:
+            code2 += "0"
+        mask >>= 1
+    code = code2
+
+    ret = dict(packet,
+               _class="command",
                protocol="sartano",
                model="codeswitch",
                code=code)
