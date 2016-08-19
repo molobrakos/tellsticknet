c29c0467970ae011cfebb9b9a2fce97c2cd240b0
Anders Sveen <anders@f12.no>
2016-08-19 20:56:14 +0200

    Introduced checksum validation

--- tellsticknet/protocols/oregon.py
+++ tellsticknet/protocols/oregon.py
@@ -14,24 +14,36 @@ def decode(packet):
     data = packet["data"]
     value = int(data)
     value >>= 8
+    checksum1 = value & 0xFF
     value >>= 8
 
+    checksum = ((value >> 4) & 0xF) + (value & 0xF)
     hum1 = value & 0xF
     value >>= 8
 
+    checksum += ((value >> 4) & 0xF) + (value & 0xF)
     neg = value & (1 << 3)
     hum2 = (value >> 4) & 0xF
     value >>= 8
 
+    checksum += ((value >> 4) & 0xF) + (value & 0xF)
     temp2 = value & 0xF
     temp1 = (value >> 4) & 0xF
     value >>= 8
 
+    checksum += ((value >> 4) & 0xF) + (value & 0xF)
     temp3 = (value >> 4) & 0xF
     value >>= 8;
 
+    checksum += ((value >> 4) & 0xF) + (value & 0xF)
     address = value & 0xFF
     value >>= 8
+
+    checksum += ((value >> 4) & 0xF) + (value & 0xF)
+    checksum += 0x1 + 0xA + 0x2 + 0xD - 0xA
+
+    if checksum != checksum:
+        raise ValueError("The checksum in the Oregon packet does not match the caluclated one!")
 
     temperature = ((temp1 * 100) + (temp2 * 10) + temp3)/10.0
     if neg:
