6fac937266e69065cd10f607f6ad99f6fdc3a804
Erik <erik.eriksson@yahoo.com>
2017-11-26 16:59:00 +0100

    Updated

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from setuptools import setup
+from os.path import exists
+
 
 setup(name='tellsticknet',
       version='0.0.1',
       description='Listen for UDP sensor broadcasts from a Tellstick',
       url='https://github.com/molobrakos/tellsticknet',
-      license='',
-      author='Erik',
-      author_email='error.errorsson@gmail.com',
-      py_modules=['tellsticknet'],
-      provides=['tellsticknet'],
+      license='?',
+      maintainer='Erik Eriksson',
+      maintainer_email='error.errorsson@gmail.com',
+      keywords='tellstick',
+      packages=['tellsticknet'],
+      long_description=(open('README.md').read() if exists('README.md') else ''),
+      install_requires=list(open('requirements.txt').read().strip().split('\n')),
       scripts=[],
-      extras_require={
-      })
+      extras_require={},
+      zip_safe=False,
+)
+
